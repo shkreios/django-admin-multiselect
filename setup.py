43e144540d840802de8712ff2432bfc866063a37
Simon Hessel <simon.hessel@kreios.lu>
2021-01-21 12:52:50 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="django-admin-multiselect", 
+    version="0.0.7",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
